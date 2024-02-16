d164a06a7e0e929ad916a70c8383a1b88dc6baad
Kieran.Dewar <Kieran.Dewar@global.com>
2024-02-16 16:04:22 +0000

    Add initial schema. Consists of one Model: Character, the Schema can either get a list of all characters or a specific character by name.

---
+++ src/main/scala/SchemaDefinition.scala
@@ -0,0 +1,14 @@
+// types
+case class Character(name: String, age: Int)
+
+def getCharacters: List[Character] = Nil
+def getCharacter(name: String): Option[Character] = Nil 
+
+
+// schema
+case class CharacterName(name: String)
+case class Queries(characters: List[Character], character: CharacterName => Option[Character])
+
+
+// resolver
+val queries = Queries(getCharacters, args => getCharacter(args.name))
